80f4b339d9501f46a4ebe7a652075ec1a8ac5ead
Kita <Akane_Kita@totec.co.jp>
2023-06-02 09:08:31 +0900

    acocounts_forms.py確認

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 #UserCreationFormクラスをインポート
 from django.contrib.auth.forms import UserCreationForm
+#models.pyで定義したカスタムUserモデルをインポート
 from .models import CustomUser
 
 
@@ -10,10 +11,10 @@ class CustomUserCreationForm(UserCreationForm):
         '''UserCreationFormのインナークラス
         Attributes:
             model:連携するUserモデル
-            fieldes:フォームで使用するフィールド
+            fields:フォームで使用するフィールド
         '''
         #連携するUserモデルを設定
         model =CustomUser
-        #ふぉーむで使用するフィールドを設定
-        #ユーザー名、メールアドレス、パスワード、パスワード確認用
+        #フォームで使用するフィールドを設定
+        #ユーザー名、メールアドレス、パスワード、パスワード(確認用)
         fields = ('username', 'email', 'password1', 'password2')
