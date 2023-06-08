c1cbdadf6d41063a264554f2d2719e12bddf6db6
takeuchi <toyohito_takeuchi@totec.co.jp>
2023-06-08 16:38:03 +0900

    リセット

--- photo/views.py
+++ photo/views.py
@@ -168,13 +168,7 @@ class DetailView(View):
         }
         return render(request, "detail.html", context)
     
-    def favocount(self,request,pk):
-        post=PhotoPost.objects.get(id=pk)
-        post.favorite=F('favorite')+1
-        post.save()
 
-        #PhotoPost.objects.filter(id=pk).update(favorite=F('favorite')+1)
-    
     """ #post.htmlをレンダリングする
     template_name = 'detail.html'
     #クラス変数modelにモデルBlogPostを設定
@@ -307,6 +301,5 @@ class subPostSuccessView(TemplateView):
     #index.htmlをレンダリングする
     template_name = 'subpost_success.html'
 
-    
-def favocount(request,pk):
-    PhotoPost.objects.filter(pk=pk).update(favorite=F('favorite')+1)
+class startview(TemplateView):
+    template_name = 'drop.html'
